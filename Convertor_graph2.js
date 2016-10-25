f84c911fbcc5ca2c2ecdc3a57e51a30d59e1e8e7
anusha <subhitcha.s85@wipro.com>
2016-10-25 16:12:07 +0530

    Json Conversion of bar graph2

--- Convertor_graph2.js
+++ Convertor_graph2.js
@@ -1,5 +1,5 @@
 /*creating a interface to read the file*/
-var read=require('fs');                                              //creating a interface to read the file
+var read=require('fs');                                             
 var lineReader = require('readline').createInterface({
 	input: read.createReadStream('data.csv'),
 });
@@ -10,34 +10,55 @@ var heading= [];
 var row=0;
 
 /*Reading the data from the csv file*/
-lineReader.on('line', function (line) 								//Reading the data from the csv file
-{
+lineReader.on('line', function (line) {
    
-   if(row===0){
-      heading =line.split(',');
+   if(row === 0){
+      heading=line.split(',');
        row++;
    }
-   else
-   {
+   else {
        var currentLineData = line.split(',');
 	   var jsonObj={};
        for (var j=0; j<heading.length; j++) {
-			if(heading[j]=="Area Name"||heading[j]=="Age-group"||heading[j]=="Literate - Persons")
-			{
-				jsonObj[heading[j]] = currentLineData[j]; 			//Copying into the JSON object	
+		   if(heading[j] == "AreaName") {
+				if(currentLineData[j+1] == "Total") {
+					jsonObj[heading[j]] = currentLineData[j];
+					count=1;
+				}
+				else{
+					count=0;
+				}
+					
+			}
+			if(heading[j] == "Age-group") {
+				if(currentLineData[j-1] == "Total") {
+					jsonObj[heading[j]] = currentLineData[j];
+					count=1;
+				}
+				else{
+					count=0;
+				}
+					
+			}
+			else if(heading[j] == "Literate-Persons") {
+				if(currentLineData[j-8] == "Total") {
+					jsonObj[heading[j]] = currentLineData[j];
+					count=1;
+				}
+				else{
+					count=0;
+				}
 			}
               
        }
-	   jsonArray.push(jsonObj);
-       
-	   
+	   if(count == 1)
+			jsonArray.push(jsonObj);   
    }
 
 });
 
 /*Appending the data into output file*/
-lineReader.on('close',function()
-{
+lineReader.on('close',function() {
 	var jso=JSON.stringify(jsonArray);
        read.appendFile('convertor_json2.json',jso,function(err) {});
 });
