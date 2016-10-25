913c1095c7e728946d642ad81f57050dcc807033
anusha <subhitcha.s85@wipro.com>
2016-10-25 16:11:11 +0530

    Json Conversion of bar graph1

--- Convertor_graph1.js
+++ Convertor_graph1.js
@@ -10,35 +10,41 @@ var heading= [];
 var row=0;
 
 /*Reading the data from the csv file*/
-lineReader.on('line', function (line)           
-{
-   if(row===0){
-		heading =line.split(',');
+lineReader.on('line', function (line)  {
+   if(row === 0){
+		heading = line.split(',');
 		row++;
    }
-   else
-   {
-	   var jsonObj={};
+   else {
+	   var jsonObj = {};
        var currentLineData = line.split(',');
        for (var j=0; j<heading.length; j++) {
-			if(heading[j]=="Area Name")
-			{
-				jsonObj[heading[j]] = currentLineData[j]; 				
+			if(heading[j] == "AreaName"){
+				if(currentLineData[j+1] == "Total" && currentLineData[j+2] == "All ages") {
+					jsonObj[heading[j]] = currentLineData[j].substring(8); 
+					count=1;
+				}
+				else
+					count=0;
 			}
-			else if(heading[j]=="Total Persons")
-			{
-				jsonObj[heading[j]] = currentLineData[j]; 			
+			else if(heading[j] == "TotalPersons") {
+				if(currentLineData[j-2] == "Total" && currentLineData[j-1] == "All ages") {
+					jsonObj[heading[j]] = currentLineData[j]; 
+					count=1;
+				}
+				else
+					count=0;
 			}
 		}
-		  jsonArray.push(jsonObj);
+		if(count == 1)
+			jsonArray.push(jsonObj);
               
     }
 
 });
 
 /*Appending the data into output file*/
-lineReader.on('close',function()
-{
+lineReader.on('close',function() {
 	var jso=JSON.stringify(jsonArray);
     read.appendFile('convertor_json1.json',jso,function(err) {});
 });
